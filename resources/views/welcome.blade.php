0b5fcf03a19dab87f71d46737633086e23c01dd5
regf12 <regf.12@gmail.com>
2019-11-24 20:31:43 -0400

    detalles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel-test-pos
                 </div>
 
                 <div class="links">
